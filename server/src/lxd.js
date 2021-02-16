ada43f6c8a2446cc1e679f582bdfafaab0399bb7
JosefLitos <invisiblemancz@gmail.com>
2021-02-16 22:11:05 +0100

    first connection of NodeJS and LXD, only for illustration purposes!

--- server/src/lxd.js
+++ server/src/lxd.js
@@ -3,19 +3,6 @@ import fs from "fs";
 import path from "path";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-/*
-let crt, key;
-fs.readFile("../config/lxcclient.crt", "utf8", (err, data) => {
-  if (err) throw err;
-  crt = data;
-  console.log("Crt: " + data);
-});
-fs.readFile("../config/lxcclient.key", "utf8", (err, data) => {
-  if (err) throw err;
-  key = data;
-  console.log("Key: " + key);
-});
-*/
 let options = {
   method: "GET",
   hostname: "127.0.0.1",
